c921bbadf47c542c390d6f2541ed5d21f0a86f67
Niklas <niklas.linz@hotmail.com>
2018-01-19 20:04:50 +0100

    .

--- src/main/java/de/linzn/mineSuite/bungee/database/DataHashTable.java
+++ src/main/java/de/linzn/mineSuite/bungee/database/DataHashTable.java
@@ -26,7 +26,7 @@ public class DataHashTable {
     public static HashMap<UUID, Long> muteTime = new HashMap<UUID, Long>();
     public static HashMap<UUID, String> mutedBy = new HashMap<UUID, String>();
     public static HashMap<UUID, String> muteReason = new HashMap<UUID, String>();
-    public static HashMap<UUID, UUID> guildInvites = new HashMap<UUID, UUID>();
+    public static HashMap<UUID, UUID> guildInvites = new HashMap<>();
     public static HashMap<UUID, UUID> msgreply = new HashMap<UUID, UUID>();
     public static HashMap<UUID, String> channel = new HashMap<UUID, String>();
     public static HashMap<UUID, Boolean> isafk = new HashMap<UUID, Boolean>();
