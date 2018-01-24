be1ec94514823fea9128e6ab2090ca6fe84515e6
Niklas <niklas.linz@hotmail.com>
2018-01-24 19:57:22 +0100

    Fix portalManager

--- src/main/java/de/linzn/mineSuite/bungee/module/portal/PortalManager.java
+++ src/main/java/de/linzn/mineSuite/bungee/module/portal/PortalManager.java
@@ -88,7 +88,7 @@ public class PortalManager {
         for (Portal portal : portalList.values()) {
             if (portal.serverName.equalsIgnoreCase(serverName)) {
                 try {
-                    Thread.sleep(10);
+                    Thread.sleep(100);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
